bfaebb63b81a49ff413c832cc093a995b706b610
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-07-11 09:58:41 -0300

    DTO standard for technician completed

--- src/main/java/com/edilson/helpdesk/domain/dtos/TechnicianDTO.java
+++ src/main/java/com/edilson/helpdesk/domain/dtos/TechnicianDTO.java
@@ -4,7 +4,10 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import com.edilson.helpdesk.domain.Technician;
+import com.edilson.helpdesk.domain.enums.Profile;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 public class TechnicianDTO implements Serializable {
@@ -19,6 +22,81 @@ public class TechnicianDTO implements Serializable {
 	
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	protected LocalDate creationDate = LocalDate.now();
+
+	public TechnicianDTO() {
+		super();
+	}
+
+	public TechnicianDTO(Technician obj) {
+		super();
+		this.id = obj.getId();
+		this.name = obj.getName();
+		this.cpf = obj.getCpf();
+		this.email = obj.getEmail();
+		this.password =obj.getPassword();
+		this.profile = obj.getProfile().stream().map(x -> x.getCode()).collect(Collectors.toSet());
+		this.creationDate = obj.getCreationDate();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Profile> getProfile() {
+		return profile.stream().map(x -> Profile.toEnum(x)).collect(Collectors.toSet());
+	}
+
+	public void addProfile(Profile profile) {
+		this.profile.add(profile.getCode());
+	}
+
+	public LocalDate getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(LocalDate creationDate) {
+		this.creationDate = creationDate;
+	}
+	
+	
+	
+	
 	
 	
 }
