c1e6df75dbdaeac73935576dae4c1e9ea3d8aaa6
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-07-16 10:12:40 -0300

    findAll for Called

--- src/main/java/com/edilson/helpdesk/services/CalledService.java
+++ src/main/java/com/edilson/helpdesk/services/CalledService.java
@@ -1,5 +1,6 @@
 package com.edilson.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,10 @@ public class CalledService {
 	public Called findById(Integer id) {
 		Optional<Called> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectnotFoundException("Object not found! ID :" + id));
+	}
+
+	public List<Called> findAll() {
+		return repository.findAll();
 	}
 	
 }
