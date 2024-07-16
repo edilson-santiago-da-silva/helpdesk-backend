43f7fad296add279c83cd6c393943f77b4fd3b86
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-07-16 09:54:15 -0300

    findById for Called - part 2

--- src/main/java/com/edilson/helpdesk/resources/CalledResource.java
+++ src/main/java/com/edilson/helpdesk/resources/CalledResource.java
@@ -1,5 +1,26 @@
 package com.edilson.helpdesk.resources;
 
-public class CalledResource {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.edilson.helpdesk.domain.Called;
+import com.edilson.helpdesk.domain.dtos.CalledDTO;
+import com.edilson.helpdesk.services.CalledService;
+
+@RestController
+@RequestMapping(value = "calleds")
+public class CalledResource {
+	
+	@Autowired
+	private CalledService service;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<CalledDTO> findById(@PathVariable Integer id){
+		Called obj = service.findById(id);
+		return ResponseEntity.ok().body(new CalledDTO(obj));
+	}
 }
--- src/main/java/com/edilson/helpdesk/services/CalledService.java
+++ src/main/java/com/edilson/helpdesk/services/CalledService.java
@@ -1,5 +1,23 @@
 package com.edilson.helpdesk.services;
 
-public class CalledService {
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.edilson.helpdesk.domain.Called;
+import com.edilson.helpdesk.repositories.CalledRepository;
+import com.edilson.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class CalledService {
+		
+	@Autowired
+	private CalledRepository repository;
+	
+	public Called findById(Integer id) {
+		Optional<Called> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectnotFoundException("Object not found! ID :" + id));
+	}
+	
 }
