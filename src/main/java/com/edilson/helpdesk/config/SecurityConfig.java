14d6ccbdc87d677bfbdfd96957e9671a927140e1
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-07-17 11:51:15 -0300

    authentication with JWT part 1

---
+++ src/main/java/com/edilson/helpdesk/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package com.edilson.helpdesk.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig { // 9:00
+
+}
