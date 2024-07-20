7f60d8874fbf03800288d58b91de1e7e08f97969
edilson-santiago-da-slva <edilsonsantiagoe7@gmail.com>
2024-07-20 08:52:22 -0300

    authorization with JWT

--- src/main/java/com/edilson/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/edilson/helpdesk/config/SecurityConfig.java
@@ -1,13 +1,14 @@
 package com.edilson.helpdesk.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,9 +20,11 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.edilson.helpdesk.security.JWTAuthenticationFilter;
+import com.edilson.helpdesk.security.JWTAuthorizationFilter;
 import com.edilson.helpdesk.security.JWTUtil;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
@@ -41,7 +44,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors(withDefaults()).csrf(csrf -> csrf.disable());
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.authorizeRequests(requests -> requests.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated());
+
         http.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 	}
 
@@ -60,7 +66,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+	BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
